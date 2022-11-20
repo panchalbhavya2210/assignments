1905aa911ba1ce74ecf6be73beb733fde7ddcb42
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-11-20 14:18:24 +0530

    commit afternoon

--- index.js
+++ index.js
@@ -9,3 +9,31 @@ function runBars() {
   barThi.classList.toggle("barThiMove");
   barBlock.classList.toggle("navCall");
 }
+
+function osOpener() {
+  let asOsOP = document.getElementById("asOsOP");
+
+  asOsOP.classList.toggle("osOpener");
+}
+
+function dbmsOpener() {
+  let asDbmsOp = document.getElementById("asDbmsOP");
+
+  asDbmsOp.classList.toggle("osOpener");
+}
+
+function cgOpener() {
+  let asCgOp = document.getElementById("asCgOP");
+
+  asCgOp.classList.toggle("osOpener");
+}
+function misOpener() {
+  let asMisOp = document.getElementById("asMisOP");
+
+  asMisOp.classList.toggle("osOpener");
+}
+function dsOpener() {
+  let asDsOp = document.getElementById("asDsOP");
+
+  asDsOp.classList.toggle("osOpener");
+}
