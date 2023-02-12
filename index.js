2197096e8e57877c302d533490a06226361759c4
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-02-12 14:27:28 +0530

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let greetUser = document.getElementById("greet-user");
+
+let getDate = new Date();
+
+let hours = getDate.getHours();
+
+if (hours == 00) {
+  greetUser.innerHTML = "Good Night";
+} else if (hours > 05 && hours < 12) {
+  greetUser.innerHTML = "Good Morning";
+} else if (hours > 12 && hours < 17) {
+  greetUser.innerHTML = "Good Afternoon";
+} else if (hours > 17 && hours < 23) {
+  greetUser.innerHTML = "Good Evening";
+}
