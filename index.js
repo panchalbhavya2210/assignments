32478e88ada5ef5b997306f7765b1e0e7395782f
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-02-19 15:34:35 +0530

    commit

--- index.js
+++ index.js
@@ -98,3 +98,33 @@ for (let i = 0; i < semFourCard.length; i++) {
     semFourDownload[i].classList.toggle("topToggle");
   });
 }
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+let dochoster = document.querySelector(".docLogo");
+
+dochoster.onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 50);
+};
